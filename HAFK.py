5ffe36d7c0bad76f5928831d913d4006e7553789
C0dwiz <codwiz@yandex.ru>
2025-03-02 01:00:41 +0300

    Feat: Add chat exclusion and message deletion features, optimize code

--- HAFK.py
+++ HAFK.py
@@ -14,23 +14,13 @@
 
 # 5. By using the Software, you agree to be bound by the terms and conditions of this license.
 
-# For any inquiries or requests for permissions, please contact codwiz@yandex.ru.
-
-# ---------------------------------------------------------------------------------
-# Name: HAFK
-# Description: Your personal assistant while you are in AFK mode
-# Author: @hikka_mods
-# ---------------------------------------------------------------------------------
-# meta developer: @hikka_mods
-# scope: HAFK
-# scope: HAFK 0.0.1
-# ---------------------------------------------------------------------------------
-
 import datetime
 import logging
 import time
+import asyncio
 
-from telethon import types  # type: ignore
+from telethon import types
+from telethon.utils import get_peer_id
 
 from .. import loader, utils
 
@@ -39,8 +29,6 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class HAFK(loader.Module):
-    """Your personal assistant while you are in AFK mode"""
-
     strings = {
         "name": "HAFK",
         "afk_on": "<emoji document_id=5357107687484038897>🫶</emoji> <b>AFK mode is on!</b>",
@@ -58,6 +46,13 @@ class HAFK(loader.Module):
         "afk_message_reason": "<emoji document_id=5330130448142049118>🫤</emoji> <b>I'm currently not accepting messages!</b>\n<b>Reason:</b> <i>{}</i>\n\n<i>Inactive mode has been on for:</i> {}",
         "ratelimit": "<b>Please wait a bit before using this command again.</b>",
         "afk_set_failed": "<b>Failed to set AFK status. Check logs.</b>",
+        "added_excluded_chat": "<b>Chat {} added to excluded chats.</b>",
+        "removed_excluded_chat": "<b>Chat {} removed from excluded chats.</b>",
+        "excluded_chats_list": "<b>Excluded chats:</b>\n{}",
+        "no_excluded_chats": "<b>No chats are excluded.</b>",
+        "invalid_chat_id": "<b>Invalid chat ID.</b>",
+        "already_excluded": "<b>Chat {} is already excluded.</b>",
+        "not_excluded": "<b>Chat {} is not excluded.</b>",
     }
 
     strings_ru = {
@@ -76,9 +71,29 @@ class HAFK(loader.Module):
         "afk_message_reason": "<emoji document_id=5330130448142049118>🫤</emoji> <b>На данный момент я не принимаю сообщения!</b>\n<b>Причина:</b> <i>{}</i>\n\n<i>С момента включения режима неактивности:</i> {}",
         "ratelimit": "<b>Пожалуйста, подождите немного перед повторным использованием этой команды.</b>",
         "afk_set_failed": "<b>Не удалось установить AFK-статус. Проверьте логи.</b>",
+        "added_excluded_chat": "<b>Чат {} добавлен в список исключений.</b>",
+        "removed_excluded_chat": "<b>Чат {} удален из списка исключений.</b>",
+        "excluded_chats_list": "<b>Список исключенных чатов:</b>\n{}",
+        "no_excluded_chats": "<b>Нет исключенных чатов.</b>",
+        "invalid_chat_id": "<b>Неверный ID чата.</b>",
+        "already_excluded": "<b>Чат {} уже исключен.</b>",
+        "not_excluded": "<b>Чат {} не исключен.</b>",
     }
 
     DEFAULT_AFK_TIMEOUT = 60
+    DEFAULT_DELETE_TIMEOUT = 5
+
+    def __init__(self):
+        self.config = loader.ModuleConfig(
+            loader.ConfigValue(
+                "excluded_chats",
+                [],
+                lambda: "List of chat IDs where AFK mode will not be activated",
+                validator=loader.validators.Series(
+                    validator=loader.validators.Integer()
+                ),
+            )
+        )
 
     async def client_ready(self, client, db):
         self.client = client
@@ -96,20 +111,19 @@ class HAFK(loader.Module):
 
     @loader.command(
         ru_doc="[reason / none] – Установить режим AFK",
+        en_doc="[reason / none] – Set AFK mode globally",
     )
     async def afk(self, message):
-        """[reason / none] – Set AFK mode globally."""
         await self._afk_toggle(message, global_afk=True)
 
     @loader.command(
         ru_doc="[reason / none] – Установить режим AFK только в этом чате.",
+        en_doc="[reason / none] – Set AFK mode in current chat only.",
     )
     async def afkhere(self, message):
-        """[reason / none] – Set AFK mode in current chat only."""
         await self._afk_toggle(message, global_afk=False)
 
     async def _afk_toggle(self, message, global_afk: bool):
-        """Toggles AFK mode, either globally or in the current chat."""
         chat_id = utils.get_chat_id(message)
         db_key = "afk" if global_afk else f"afk_here_{chat_id}"
         already_afk_string = "already_afk" if global_afk else "already_afk_here"
@@ -121,7 +135,6 @@ class HAFK(loader.Module):
             return
 
         reason = utils.get_args_raw(message) or None
-
         success = self._set_afk(
             True, reason=reason, chat_id=chat_id if not global_afk else None
         )
@@ -130,31 +143,29 @@ class HAFK(loader.Module):
             await utils.answer(message, self.strings("afk_set_failed", message))
             return
 
-        if reason:
-            await utils.answer(
-                message, self.strings(afk_on_reason_string, message).format(reason)
-            )
-        else:
-            await utils.answer(message, self.strings(afk_on_string, message))
+        await utils.answer(
+            message,
+            self.strings(afk_on_reason_string, message).format(reason)
+            if reason
+            else self.strings(afk_on_string, message),
+        )
 
     @loader.command(
         ru_doc="Выйти из режима AFK",
+        en_doc="Exit AFK mode",
     )
     async def unafk(self, message):
-        """Exit AFK mode"""
         await self._unafk_toggle(message, global_afk=True)
 
     @loader.command(
         ru_doc="Выйти из режима AFK в этом чате",
+        en_doc="Exit AFK mode in this chat",
     )
     async def unafkhere(self, message):
-        """Exit AFK mode in this chat"""
         await self._unafk_toggle(message, global_afk=False)
 
     async def _unafk_toggle(self, message, global_afk: bool):
-        """Turns off AFK mode, either globally or in the current chat."""
         chat_id = utils.get_chat_id(message)
-        db_key = "afk" if global_afk else f"afk_here_{chat_id}"
         not_afk_string = "not_afk" if global_afk else "not_afk_here"
         afk_off_time_string = "afk_off_time" if global_afk else "afk_off_here_time"
 
@@ -162,9 +173,9 @@ class HAFK(loader.Module):
             await utils.answer(message, self.strings(not_afk_string, message))
             return
 
-        now = datetime.datetime.now().replace(microsecond=0)
         total_gone_time = self._calculate_total_afk_time(
-            now, chat_id=chat_id if not global_afk else None
+            datetime.datetime.now().replace(microsecond=0),
+            chat_id=chat_id if not global_afk else None,
         )
 
         self._set_afk(False, chat_id=chat_id if not global_afk else None)
@@ -178,69 +189,60 @@ class HAFK(loader.Module):
         if not isinstance(message, types.Message):
             return
 
-        chat_id = utils.get_chat_id(message)
+        chat_id = get_peer_id(message.peer_id)
         user_id = getattr(message.to_id, "user_id", None)
         is_mentioned = message.mentioned or user_id == self._me.id
         is_private = isinstance(message.to_id, types.PeerUser)
 
-        if not (is_mentioned or is_private):
+        if not (is_mentioned or is_private) or chat_id in self.config["excluded_chats"]:
             return
 
         reason = None
-        afk_state = False
         gone_time = None
 
-        is_afk_here = self._is_afk_enabled(chat_id, False)
-        if is_afk_here:
+        if self._is_afk_enabled(chat_id, False):
             reason = self.db.get(__name__, f"afk_here_{chat_id}_reason", None)
-            afk_state = True
             gone_time = self.db.get(__name__, f"gone_afk_here_{chat_id}", None)
             ratelimit_key = f"ratelimit_here_{chat_id}"
         elif self.global_afk:
             reason = self.global_afk_reason
-            afk_state = True
             gone_time = self.global_gone_time
             ratelimit_key = "ratelimit"
         else:
             return
 
-        if afk_state:
-            if gone_time is None:
-                logger.warning(
-                    f"No 'gone' time found in database for {ratelimit_key}. Cannot send AFK message."
-                )
-                return
+        if gone_time is None:
+            logger.warning(
+                f"No 'gone' time found for {ratelimit_key}. Cannot send AFK."
+            )
+            return
 
-            if self._ratelimit(chat_id, ratelimit_key):
-                await self._send_afk_message(message, reason, gone_time)
-            else:
-                await utils.answer(message, self.strings("ratelimit", message))
+        if not self._ratelimit(chat_id, ratelimit_key):
+            await utils.answer(message, self.strings("ratelimit", message))
+            return
+
+        afk_message = await self._send_afk_message(message, reason, gone_time)
+        if afk_message:
+            await self._delete_message(afk_message, self.DEFAULT_DELETE_TIMEOUT)
 
     def _set_afk(self, value: bool, reason: str = None, chat_id: int = None) -> bool:
-        """Sets the AFK status in the database and updates class variables. Returns True on success, False on failure."""
         try:
             key_prefix = f"afk_here_{chat_id}" if chat_id else "afk"
-
             self.db.set(__name__, key_prefix, value)
             self.db.set(__name__, f"{key_prefix}_reason", reason)
 
             gone_key = f"gone_{key_prefix}"
-            if value:
-                gone_time = time.time()
-                self.db.set(__name__, gone_key, gone_time)
-                logger.debug(f"AFK enabled. {gone_key} set to {gone_time}")
-            else:
-                self.db.set(__name__, gone_key, None)
-                logger.debug(f"AFK disabled. {gone_key} set to None")
+            gone_time = time.time() if value else None
+            self.db.set(__name__, gone_key, gone_time)
+            logger.debug(f"AFK status updated. {gone_key} set to {gone_time}")
 
             if chat_id is None:
                 self.global_afk = value
                 self.global_afk_reason = reason
-                self.global_gone_time = gone_time if value else None
+                self.global_gone_time = gone_time
                 logger.debug("Updated global AFK vars")
 
             return True
-
         except Exception as e:
             logger.exception(f"Error setting AFK status: {e}")
             return False
@@ -248,7 +250,6 @@ class HAFK(loader.Module):
     def _calculate_total_afk_time(
         self, now: datetime.datetime, chat_id: int = None
     ) -> datetime.timedelta:
-        """Calculates the total time spent in AFK mode."""
         key_prefix = f"afk_here_{chat_id}" if chat_id else "afk"
         gone_time = self.db.get(__name__, f"gone_{key_prefix}")
 
@@ -257,14 +258,12 @@ class HAFK(loader.Module):
 
         try:
             gone = datetime.datetime.fromtimestamp(gone_time).replace(microsecond=0)
-            total_gone = (now - gone).total_seconds()
-            return datetime.timedelta(seconds=total_gone)
+            return now - gone
         except Exception as e:
             logger.error(f"Error calculating AFK time: {e}")
             return datetime.timedelta(seconds=0)
 
     def _ratelimit(self, chat_id: int, key: str) -> bool:
-        """Ratelimits AFK messages to avoid spam."""
         now = time.time()
         last_sent = self._ratelimit_cache.get((chat_id, key), 0)
 
@@ -275,43 +274,42 @@ class HAFK(loader.Module):
         return True
 
     async def _send_afk_message(self, message, reason, gone_time):
-        """Sends the AFK message to the user."""
         user = await utils.get_user(message)
         if user.is_self or user.bot or user.verified:
             logger.debug("User is self, bot, or verified. Not sending AFK message.")
-            return
-
-        if gone_time is None:
-            logger.warning(
-                "No 'gone' time found in database.  Cannot calculate AFK duration."
-            )
-            return
+            return None
 
         now = datetime.datetime.now().replace(microsecond=0)
         try:
             gone = datetime.datetime.fromtimestamp(gone_time).replace(microsecond=0)
         except (TypeError, ValueError) as e:
             logger.error(f"Error converting timestamp: {e}")
-            return
+            return None
 
-        diff = now - gone
-        time_string = str(diff)
-
-        if reason:
-            ret = self.strings("afk_message_reason", message).format(
-                reason, time_string
-            )
-        else:
-            ret = self.strings("afk_message", message).format(time_string)
+        time_string = str(now - gone)
+        ret = (
+            self.strings("afk_message_reason", message).format(reason, time_string)
+            if reason
+            else self.strings("afk_message", message).format(time_string)
+        )
 
         try:
-            await utils.answer(message, ret, reply_to=message)
+            reply = await utils.answer(message, ret, reply_to=message)
+            return reply
         except Exception as e:
             logger.exception(f"Error sending AFK message: {e}")
+            return None
 
     def _is_afk_enabled(self, chat_id: int = None, global_afk: bool = False) -> bool:
-        """Helper function to check if AFK is enabled, either globally or for a specific chat."""
-        if global_afk:
-            return self.global_afk
-        else:
-            return self.db.get(__name__, f"afk_here_{chat_id}", False)
+        return (
+            self.global_afk
+            if global_afk
+            else self.db.get(__name__, f"afk_here_{chat_id}", False)
+        )
+
+    async def _delete_message(self, message, delay):
+        await asyncio.sleep(delay)
+        try:
+            await self.client.delete_messages(message.chat_id, message.id)
+        except Exception as e:
+            logger.exception(f"Error deleting message: {e}")
